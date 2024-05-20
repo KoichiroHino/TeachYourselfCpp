42c6db671d979967bb099673eedee8ebef5f3660
masayuki gondo <masayukigondo@system76-pc.localdomain>
2024-05-20 22:41:35 +0900

    クラスの宣言方法とポリモフィズムの有無による差分を追加

--- Chapter3-class/src/main.cpp
+++ Chapter3-class/src/main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 #include <string>
 
+// check the difference between polymorphism and non
+#define POLYMORPHISM
+
 class person
 {
+
+protected:
     std::string m_name;
     int m_age;
 
@@ -13,13 +18,30 @@ public:
     person(std::string name, int age);
 
     person(const person& other); // コピーコンストラクター
-
+#ifdef POLYMORPHISM
+    virtual void set_name(std::string name);
+#else
     void set_name(std::string name);
+#endif
     void set_age(int age);
 
     std::string name() const;
     int age() const;
 };
+
+class SuperPerson : public person
+{
+    public:
+    #ifdef POLYMORPHISM
+        void set_name(std::string name) override;
+    #else
+        void set_name(std::string name);
+    #endif
+};
+
+void SuperPerson::set_name(std::string name){
+    m_name = "super " + name;
+}
 
 person::person(int age) : m_age(age)
 {
@@ -101,11 +123,28 @@ int main()
     // コピーコンストラクタ
     person alice("alice", 15);
 
+    // クラスの宣言
+    std::cout << "test" << std::endl;
+    person person1 = person();
+    person person2;
+    std::cout << "test" << std::endl;
+
     person copy(alice); // コピーコンストラクター呼び出し
     // person copy = alice;  // このように書いてもコピーコンストラクタが呼び出される
     std::cout << copy.name() << std::endl;
     std::cout << alice.name() << std::endl;
 
+    // ポリモーフィズムで動きの変わる処理
+    #ifdef POLYMORPHISM
+    person *sp = new SuperPerson();
+    sp->set_name("aaa");
+    std::cout << "name: " << sp->name() << std::endl;
+
+    #else
+    SuperPerson sp2;
+    sp2.set_name("aaa");
+    std::cout << "name: " << sp2.name() << std::endl;    
+    #endif
     // staticクラスメンバ
     // privateなのでクラス外からのアクセスは出来ない
     // std::cout << S::count << std::endl;
